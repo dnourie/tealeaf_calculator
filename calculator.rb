116e61bb3432aa708624404b3dc51ce9f2d47ba9
dnourie <dananourie@gmail.com>
2014-09-18 18:07:50 -0700

    calculator app

--- calculator.rb
+++ calculator.rb
@@ -2,7 +2,7 @@
 
 # Get two numbers from the user via command line
 puts "Type go if you want to calculate. Type exit, if you want to exit."
-response = gets.chomp
+response = gets.chomp.downcase
   until response == "exit" do
     puts "Enter the first number:"
     num1 = gets.chomp
@@ -10,22 +10,24 @@ response = gets.chomp
     num2 = gets.chomp
 
     # Ask the user which operator to use
-    puts "Do you want to add, subtract, multiply, or divide the numbers?"
+    puts "Type the math operator you want: +, -, *, /"
     operator = gets.chomp
 
     # Perform the calculation for the numbers through operator given
     case 
-      when operator == 'add'
+      when operator == '+'
         total = num1.to_i + num2.to_i
-      when operator == 'subtract'
+      when operator == '-'
         total = num1.to_i - num2.to_i
-      when operator == 'multiply'
+      when operator == '*'
         total = num1.to_i * num2.to_i
-      when operator == 'divide'
+      when operator == '/'
         total = num1.to_f / num2.to_f
+      when operator == 'sqrt'
+        total = num1.to_f 
     end
     # Return the results
     puts "Your total is #{total}!"
     puts "Type exit if you are done!"
-    response = gets.chomp
+    response = gets.chomp.downcase
   end
